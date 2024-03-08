d2d9c5d18bd0a6c0ba1a95408a7ba87bf649ed93
Guterresthomas <guterresmikael@gmail.com>
2024-03-08 11:45:57 -0300

    ajustes e adições

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -5,7 +5,7 @@ export const Features = (props) => {
     <div id="features" className="text-center">
       <div className="container">
         <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Benefícios de estar aqui</h2>
+          <h2>BENEFÍCIOS DE SER ESTAR AQUI E SER NOSSO ALUNO(a)</h2>
         </div>
         <div className="row">
           {props.data
